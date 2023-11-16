2d1a7417b1fa352727b58f15cb4fae233ffa56bd
Oki Okwiri <111518495+okiokwiri@users.noreply.github.com>
2023-11-16 10:50:17 +0300

    added tmr2 files

--- src/bsp/Tmr1.h
+++
@@ -1,5 +0,0 @@
-#ifndef TMR1_H
-#define TMR1_H
-
-
-#endif // TMR1_H
--- test/bsp/testable_micro_registers.c
+++ test/bsp/testable_micro_registers.c
@@ -6,3 +6,5 @@ volatile GPIO_TypeDef GPIOA_BASE;
 volatile GPIO_TypeDef* GPIOA = &GPIOA_BASE;
 volatile USART_TypeDef UART5_BASE;
 volatile USART_TypeDef* UART5 = &UART5_BASE;
+volatile TIM_TypeDef TIM2_BASE;
+volatile TIM_TypeDef* TIM2 = &TIM2_BASE;
